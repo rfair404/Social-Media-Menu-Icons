56bf0e1478356fba2db05056f718a22a20e9ceea
Russell Fair <rfair404@gmail.com>
2014-05-25 12:12:39 -0400

    add all the files this time

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,17 +2,6 @@
 module.exports = function(grunt) {
 
     grunt.initConfig({
-        jshint: {
-              options: {
-                    jshintrc: '.jshintrc'
-              },
-              all: [
-                    'Gruntfile.js',
-                    'lib/admin/js/*.js',
-                    'lib/frontend/js/*.js',
-                    '!lib/admin/js/scripts.min.js'
-              ]
-        },
         less: {
             dist: {
                 options: {
@@ -30,22 +19,16 @@ module.exports = function(grunt) {
             less: {
                 files: [
                     'lib/less/*.less',
+                    'lib/thirdparty/font-awesome/less/*.less',
                 ],
                 tasks: ['less']
             },
-            js : {
-                files: [
-                    'lib/js/*.js',
-                ]
-            }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-less');
 
     grunt.registerTask('dev', ['watch']);
-    grunt.registerTask('compile', ['jshint', 'less']);
 };
--- social-media-menu-icons.php
+++ social-media-menu-icons.php
@@ -8,10 +8,18 @@
 add_action('init', 'smmi_register_styles');
 function smmi_register_styles(){
     $version = (int) '203';
-    wp_register_style('smmi', plugins_url( 'lib/css/smmi.css' , __FILE__ ), array(), $version, 'screen');
+    wp_register_style('smmi-fa', '//netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css', array(), '410', 'screen');
+    wp_register_style('smmi', plugins_url( 'lib/css/smmi.min.css' , __FILE__ ), array('smmi-fa'), $version, 'screen');
 }
 
 add_action('wp_print_scripts', 'smmi_output_css');
 function smmi_output_css(){
-    wp_enqueue_style('smmi');
+    if(!is_admin())
+        wp_enqueue_style('smmi');
+}
+
+add_filter('wp_nav_menu_args', 'smmi_menu_args', 1, 10);
+function smmi_menu_args($args){
+    $args['menu_class'] = $args['menu_class'] . ' smmi-navigation';
+    return $args;
 }
